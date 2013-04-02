d0cdc972c674049d35d35580e8a769bcac48085e
Conrad Meyer <cse.cem@gmail.com>
2013-04-01 22:12:50 -0700

    Updated block_bench to dump hash of known string...

--- block_bench.c
+++ block_bench.c
@@ -16,8 +16,12 @@
 /*
  * To bench C implementation, change to '1'; to bench ASM, compile against
  * skein_block_<whatever>.s
+ *
+ * The problem with the ASM implementation is that it produces incorrect
+ * hashes. Maybe pre-1.3?
  */
-#if 0
+#if 1
+# define SKEIN_UNROLL_1024 10
 # include "skein_block.c"
 #endif
 #include "skein.c"
@@ -31,9 +35,8 @@ ASSERT(intptr_t i)
 }
 
 inline void
-hash(const uint8_t *trial, size_t len)
+hash(const uint8_t *trial, size_t len, void *out)
 {
-	uint8_t trhash[1024/8];
 	Skein1024_Ctxt_t c;
 	int r;
 
@@ -43,7 +46,7 @@ hash(const uint8_t *trial, size_t len)
 	r = Skein1024_Update(&c, (void*)trial, len);
 	ASSERT(r == SKEIN_SUCCESS);
 
-	r = Skein1024_Final(&c, trhash);
+	r = Skein1024_Final(&c, out);
 	ASSERT(r == SKEIN_SUCCESS);
 }
 
@@ -81,6 +84,22 @@ ascii_incr(char *str)
 	}
 }
 
+void
+dump_hex(void *v, size_t sz)
+{
+	uint32_t *u32 = v;
+
+	ASSERT(sz % sizeof(*u32) == 0);
+
+	for (; sz > 0; sz -= sizeof *u32) {
+		printf("%08x", be32toh(*u32));
+		u32++;
+	}
+
+	printf("\n");
+	fflush(stdout);
+}
+
 int
 main(void)
 {
@@ -90,6 +109,7 @@ main(void)
 	const uint64_t NTRIALS = 1000000;
 	int rc;
 	struct timespec begin, end;
+	uint8_t trhash[1024/8];
 
 	status[0] = 'A';
 
@@ -101,7 +121,7 @@ main(void)
 		ASSERT(rc == 0);
 
 		for (uint64_t t = 0; t < NTRIALS; t++) {
-			hash((uint8_t*)status, len);
+			hash((uint8_t*)status, len, trhash);
 
 			overflow = ascii_incr(status);
 			if (overflow) {
@@ -121,6 +141,10 @@ main(void)
 		hps /= seconds;
 		printf("Trial %u Skeins/sec: %.02f\n", i, hps);
 	}
+
+	hash((void*)"HnFZBl", 6, trhash);
+	printf("Hash(HnFZBl) = ");
+	dump_hex(trhash, sizeof trhash);
 
 	return 0;
 }
